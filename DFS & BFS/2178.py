6a44d6cb080cd14cfd72df46e1bbdbbe12b2cbc7
dnwjddl <woojung980305@gmail.com>
2021-11-05 12:46:55 +0900

    Create 2178.py

---
+++ DFS & BFS/2178.py
@@ -0,0 +1,26 @@
+from collections import deque
+
+N, M = map(int, input().split())
+graph = [list(map(int, list(input()))) for _ in range(N)]
+
+dx = [-1, 0, 1, 0]
+dy = [0, -1, 0, 1]
+
+visited = [[0] * M for _ in range(N)]
+
+q = deque([(0, 0)])
+visited[0][0] = 1
+
+while q:
+    x, y = q.popleft()
+
+    if x == N-1 and y == M-1:
+        print(visited[x][y])
+    
+    for i in range(4):
+        nx, ny = x + dx[i], y + dy[i]
+
+        if 0 <= nx < N and 0 <= ny < M:
+            if visited[nx][ny] == 0 and graph[nx][ny] == 1:
+                visited[nx][ny] = visited[x][y] + 1
+                q.append((nx, ny))
